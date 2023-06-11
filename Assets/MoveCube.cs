b680a9ba1c6c75b4722ceab0225215c333d59539
alexs <alexs@ICEYE>
2023-06-11 14:27:15 +0300

    Проверка

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -1,112 +1,71 @@
 using UnityEngine;
-using System.Collections;
-using System;
-using System.Text.RegularExpressions;
 
-//этот скрипт вешаем на непосредственно на камеру
-
-public class MovementObjects : MonoBehaviour
+public class Grabber : MonoBehaviour
 {
-    public int ID;
-    public int layerObj;
-    public bool memoryLayer;
-    public float yObj;
-    public string nameIgnore;
 
-    public float hitPoint;
+    private GameObject selectedObject;
 
-
-    RaycastHit hit;
-    Ray ray;
-
-    public GameObject obj;
-
-    void Start()
+    private void Update()
     {
-        memoryLayer = true;
-    }
-
-    void Update()
-    {
-        Motionobject();
-    }
-
-    //проверка перетаскиваемых объектов осуществляется по ID в имени объекта
-    void Motionobject()
-    {
-        //проверяем нажатие кнопки мышки
         if (Input.GetMouseButtonDown(0))
         {
-            //задаем направление луча
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            //пускаем луч бесконечной длинны и если попали куда то, то
-            if (Physics.Raycast(ray, out hit))
+            if (selectedObject == null)
             {
-                //проверяем есть ли в имени десятичные цифры
-                var reg = @"[0-9]+";
+                RaycastHit hit = CastRay();
 
-                //получаем имя того во что попал райкаст и убираем у него приставку ID
-                nameIgnore = hit.collider.gameObject.name.Replace("ID", "");
-
-                Debug.Log("Regex.IsMatch(nameIgnore, reg) = " + Regex.IsMatch(nameIgnore, reg));
-
-                if (Regex.IsMatch(nameIgnore, reg))
+                if (hit.collider != null)
                 {
-                    //Debug.Log("hit.collider.name = " + nameIgnore);
-
-                    //и конвектируем в числовое значение
-                    ID = Convert.ToInt32(nameIgnore);
-
-                    if (ID != 1)
+                    if (!hit.collider.CompareTag("drag"))
                     {
-                        //запоминаем во что попали лучем, для дальнейшей работы с этим ГО
-                        obj = hit.collider.gameObject;
-                        Debug.Log("1");
+                        return;
                     }
+
+                    selectedObject = hit.collider.gameObject;
+                    Cursor.visible = false;
                 }
             }
-        }
-
-        //тут мы и будем перемещать наш ГО
-        if (obj != null)
-        {
-            Ray();
-
-            //ставим флаг что уже запомнили слой
-            if (memoryLayer)
+            else
             {
-                //запоминаем слой ГО
-                layerObj = obj.layer;
-                memoryLayer = false;
+                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+                selectedObject.transform.position = new Vector3(worldPosition.x, 0f, worldPosition.z);
+
+                selectedObject = null;
+                Cursor.visible = true;
             }
-
-            //делаем ГО проницаемым для луча, что бы перемещать его в точку падения луча на терейне
-            obj.layer = 2;
-            //Debug.Log("hit.point = " + hit.point);
-
-            //присваиваем их объекту на который попал луч
-            //перемещаем соответственно только по горизонтальным плоскостям,
-            //new Vector3(0, 0.5f, 0); приподнимаем ГО что бы он не сидел в текстурах
-            //если подставить за место hit.distance например 10, то ГО будет двигаться на растоянии 10
-            obj.transform.position = ray.GetPoint(hit.distance) + new Vector3(0, 0.5f, 0);
         }
 
-        //если кнопка была отжата то перестаем работать с ГО и обнуляем переменную, возвращаем слой обратно какой был
-        if (Input.GetMouseButtonUp(0))
+        if (selectedObject != null)
         {
-            if (obj != null)
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+            selectedObject.transform.position = new Vector3(worldPosition.x, .25f, worldPosition.z);
+
+            if (Input.GetMouseButtonDown(1))
             {
-                obj.layer = layerObj;
-                memoryLayer = true;
-                obj = null;
+                selectedObject.transform.rotation = Quaternion.Euler(new Vector3(
+                    selectedObject.transform.rotation.eulerAngles.x,
+                    selectedObject.transform.rotation.eulerAngles.y + 90f,
+                    selectedObject.transform.rotation.eulerAngles.z));
             }
         }
     }
 
-    void Ray()
+    private RaycastHit CastRay()
     {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit);
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.farClipPlane);
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
+
+        return hit;
     }
 }
